edebcc23781f9416bf0a536622f585c0bd5404d7
Dmitriy <albatrozzz1@gmail.com>
2023-01-05 15:43:03 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        
+                    }
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    },
+    devServer: {
+        port: 8080,
+        open: true,
+        stats: 'errors-only'
+    }
+};
